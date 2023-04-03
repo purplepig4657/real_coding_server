542c3e0bd1c45ae79563518d5ff26c6b689fc24a
purplepig4657 <80941516+purplepig4657@users.noreply.github.com>
2023-04-03 17:35:09 +0900

    202102632 done

--- src/main/java/com/cnu/real_coding_server/controller/PostController.java
+++ src/main/java/com/cnu/real_coding_server/controller/PostController.java
@@ -1,7 +1,50 @@
 package com.cnu.real_coding_server.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.cnu.real_coding_server.entity.Post;
+import com.cnu.real_coding_server.model.request.PostRequest;
+import com.cnu.real_coding_server.service.PostService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("posts")
 public class PostController {
+
+    private final PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @GetMapping("/{postId}")
+    public ResponseEntity<Post> getPost(@PathVariable("postId") Integer postId) {
+        return ResponseEntity.ok(this.postService.getPost(postId).orElse(null));
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<Post>> getPosts() {
+        return ResponseEntity.ok(this.postService.getPosts());
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<Post> createPost(@RequestBody PostRequest postRequest) {
+        return ResponseEntity.ok(this.postService.createPost(postRequest.toEntity()));
+    }
+
+    @PutMapping("/{postId}")
+    public ResponseEntity<Post> updatePost(
+            @PathVariable("postId") Integer postId,
+            @RequestBody PostRequest postRequest
+    ) {
+        return ResponseEntity.ok(this.postService.updatePost(postId, postRequest.toEntity()));
+    }
+
+    @DeleteMapping("/{postId}")
+    public ResponseEntity<Void> deletePost(@PathVariable("postId") Integer postId) {
+        this.postService.deletePost(postId);
+        return ResponseEntity.noContent().build();
+    }
+
 }
--- src/main/java/com/cnu/real_coding_server/service/PostService.java
+++ src/main/java/com/cnu/real_coding_server/service/PostService.java
@@ -1,7 +1,47 @@
 package com.cnu.real_coding_server.service;
 
+import com.cnu.real_coding_server.entity.Post;
+import com.cnu.real_coding_server.repository.PostRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
+@Transactional
 public class PostService {
+
+    private final PostRepository postRepository;
+
+    public PostService(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    public Optional<Post> getPost(Integer postId) {
+        return this.postRepository.findById(postId);
+    }
+
+    public List<Post> getPosts() {
+        return this.postRepository.findAll();
+    }
+
+    public Post createPost(Post post) {
+        return this.postRepository.save(post);
+    }
+
+    public Post updatePost(Integer postId, Post updatedPost) {
+        Post targetPost = this.postRepository.findById(postId).orElse(null);
+        Assert.notNull(targetPost, "target post must not be null.");
+        targetPost.setTitle(updatedPost.getTitle());
+        targetPost.setContents(updatedPost.getContents());
+        targetPost.setTag(updatedPost.getTag());
+        return targetPost;
+    }
+
+    public void deletePost(Integer targetPostId) {
+        Optional<Post> targetPost = this.postRepository.findById(targetPostId);
+        targetPost.ifPresent(this.postRepository::delete);
+    }
 }
